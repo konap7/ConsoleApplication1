bcd97804d27a7e4c5aeeae9961e050ee4b140bce
Simon Llanso <cogliostro_g@hotmail.com>
2016-02-15 14:31:57 +0100

    Kommentar

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mata in tal 1: ");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nMata in tal 2: ");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+
+            return Adddera(ta1, tal2);
+        }
+
+        static void Addera (int tal1, int tal2)
+        {
+            return tal1 + tal2;
+        }
+    }
+}
